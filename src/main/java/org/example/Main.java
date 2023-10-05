1986b0f0dc9e0386ca8c2b18fbb070a1213a145f
Evie Omede <eomede@appdev.yearup.com>
2023-10-05 17:20:05 -0400

    edit code to print shopping list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("shopping-list");
+        System.out.println("shopping-list");
+        System.out.println("shopping-list");
+        System.out.println("matcha powder");
+        System.out.println("oranges");
+        System.out.println("avacados");
+        System.out.println("lemons");
     }
 }
